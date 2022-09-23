45d25f6562796a9efde0a5219bf9169005d129e9
JhonataCosta13 <costajhonata8@gmail.com>
2022-09-22 21:12:19 -0300

    repositorio

---
+++ src/main/java/com/example/fontes/repository/EquipamentosRepository.java
@@ -0,0 +1,10 @@
+package com.example.fontes.repository;
+
+import com.example.fontes.model.Equipamentos;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EquipamentosRepository extends JpaRepository<Equipamentos, Long> {
+
+}
--- src/main/java/com/example/fontes/service/EquipamentosServiceImpl.java
+++ src/main/java/com/example/fontes/service/EquipamentosServiceImpl.java
@@ -1,12 +1,18 @@
 package com.example.fontes.service;
 
 import com.example.fontes.model.Equipamentos;
+import com.example.fontes.repository.EquipamentosRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 public class EquipamentosServiceImpl implements EquipamentosService {
+    @Autowired
+    private EquipamentosRepository equipamentosRepository;
     @Override
+
     public Equipamentos encontrarPorId(Long id) {
+
         return null;
     }
 
